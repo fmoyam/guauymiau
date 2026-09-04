4b9fc64ae7fdacb44f3490f06145497fec05c1d0
fmoyam <fel.moyam@duocuc.cl>
2026-09-03 20:40:54 -0400

    modificacion al map de usuarios + alerta de login exitoso

--- js/login.js
+++ js/login.js
@@ -1,8 +1,34 @@
 const usuarios = new Map();
 
-usuarios.set("felip.fernandez@duocuc.cl", "ColoColo");
-usuarios.set("felip.moya@duocuc.cl", "Felipe");
-usuarios.set("javiera.banares@duocuc.cl", "Javiera");
+usuarios.set("felip.fernandez@duocuc.cl", {
+  password: "ColoColo",
+  nombre: "Felipe Fernandez",
+  telefono: "+56912345678",
+  mascotas: [
+    { especie: "perro", nombre: "Lala" },
+    { especie: "gato", nombre: "Lolo" }
+  ]
+});
+
+usuarios.set("felip.moya@duocuc.cl", {
+  password: "fmoyam",
+  nombre: "Felipe Moya",
+  telefono: "+56931435167",
+  mascotas: [
+    { especie: "gato", nombre: "Crema" }
+  ]
+});
+
+usuarios.set("javiera.banares@duocuc.cl", {
+  password: "jbanares",
+  nombre: "Javiera Banares",
+  telefono: "+56911223344",
+  mascotas: [
+    { especie: "hamster", nombre: "Toto" },
+    { especie: "rata", nombre: "Ninja" },
+    { especie: "perro", nombre: "Cachupin" }
+  ]
+});
 
 const Login = document.querySelector("#form-login");
 
@@ -17,13 +43,17 @@ Login.addEventListener("submit", (e) => {
     return;
   }
 
-  const contraCorrecta = usuarios.get(email);
+  const usuario = usuarios.get(email);
 
-  if (contraCorrecta !== contra) {
+  if (usuario.password !== contra) {
     alert("Contraseña incorrecta.");
     return;
   }
 
-  window.location.href = "index.html"
-
+  let pets = "Mascotas de " + usuario.nombre + ":\n";
+    for (let i = 0; i < usuario.mascotas.length; i++) {
+      pets += (i + 1) + ". " + usuario.mascotas[i].especie + " - " + usuario.mascotas[i].nombre + "\n";
+    }
+  alert(pets);
+  window.location.href = "index.html";
 });
